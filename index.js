14aff0a6794e6fa5e8ebe67fe4c8f599abfb8431
Rudra <rudrajit2009@outlook.com>
2024-04-29 14:18:10 +0530

    Responsive + score system + results

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const startScreen = document.querySelector(".mainScreen");
 const startBTN = document.querySelector(".startBTN");
 const gameContainer = document.querySelector(".gameContainer");
-const playerMoves = document.querySelectorAll('.playerMoves')
+const playerMoves = document.querySelectorAll(".playerMoves");
 const playerChoiceDisplay = document.querySelector(".playerChoiceDisplay");
 const compChoiceDisplay = document.querySelector(".compChoiceDisplay");
 const playerScoreDisplay = document.querySelector(".playerScoreDisplay");
 const compScoreDisplay = document.querySelector(".compScoreDisplay");
 const resultDisplay = document.querySelector(".resultDisplay");
-const winLoseText = document.querySelector('.condition');
-  
+const winLoseText = document.querySelector(".condition");
+
 startBTN.addEventListener("click", () => {
   let audio = new Audio("sounds/click.wav");
   audio.currentTime = 0;
@@ -22,19 +22,42 @@ startBTN.addEventListener("click", () => {
   startScreen.remove();
 });
 
-playerMoves.forEach((Element)=>{
-  Element.addEventListener('click',()=>{
-    let audio = new Audio("sounds/click.wav");
-    audio.currentTime = 0;
-    audio.play();
-    if(Element.id === 'rock'){
-      playerChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand-back-fist"></i>`
-    }
-    if(Element.id === 'paper'){
-      playerChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand"></i>`
-    }
-    if(Element.id === 'scissor'){
-      playerChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand-scissors"></i>`
-    }
-  })
-})
+function game() {
+  playerMoves.forEach((Element) => {
+    Element.addEventListener("click", () => {
+      let audio = new Audio("sounds/click.wav");
+      audio.currentTime = 0;
+      audio.play();
+      if (Element.id === "rock") {
+        playerChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand-back-fist"></i>`;
+      }
+      if (Element.id === "paper") {
+        playerChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand"></i>`;
+      }
+      if (Element.id === "scissor") {
+        playerChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand-scissors"></i>`;
+      }
+      let randomNumber = Math.floor(Math.random() * 3 + 1);
+      if (randomNumber === 1) {
+        compChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand-back-fist"></i>`;
+      }
+      if (randomNumber === 2) {
+        compChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand"></i>`;
+      }
+      if (randomNumber === 3) {
+        compChoiceDisplay.innerHTML = `<i class="fa-regular fa-hand-scissors"></i>`;
+      }
+      if (playerChoiceDisplay.innerHTML === compChoiceDisplay.innerHTML) {
+        console.log("Tie");
+      }
+      if (
+        (Element.id === 'rock' && randomNumber === 3) ||
+        (Element.id === 'scissors' && randomNumber === 2) ||
+        (Element.id === 'paper' && randomNumber === 1)
+      ) {
+        console.log('You gained a point!');
+      }
+    });
+  });
+}
+game();
